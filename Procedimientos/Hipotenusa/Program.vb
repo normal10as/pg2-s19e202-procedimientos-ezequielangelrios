12dca822e60344517fc6eaa8954bf78b803fd611
ezequiel rios <riosezequielangel@gmail.com>
2019-05-18 23:51:33 -0300

    hipotenusa

--- Procedimientos/Hipotenusa/Program.vb
+++ Procedimientos/Hipotenusa/Program.vb
@@ -2,11 +2,15 @@ Imports System
 Imports System.Math
 Module Program
     Sub Main(args As String())
-        'ingresar los valores
-        'llamar la funcion
-        'imprimir
+        Dim LadoA, LadoB As UInt16
+
+        Console.WriteLine("Ingrese los valores de los lados A; B: ")
+        LadoA = Console.ReadLine()
+        LadoB = Console.ReadLine()
+        Console.WriteLine("Valor del lado C: " & CalculoC(LadoA, LadoB))
+
     End Sub
     Function CalculoC(a As UInt16, b As UInt16) As UInt16
-        Return Sqrt(a)
+        Return Sqrt((a ^ 2) + (b ^ 2))
     End Function
 End Module
